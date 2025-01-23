cbbeb92785a25b05a3783045fb39b6c270877746
ReynaldoMolina <molinareynaldo952@gmail.com>
2025-01-23 07:01:49 -0600

    changed root directory, loading component and index

--- src/ToDoLoading/index.js
+++ src/ToDoLoading/index.js
@@ -1,13 +1,12 @@
-import { ReactComponent as SVGLoading} from './icon-loading.svg';
-// import flork from './flork.webp';
 import './ToDoLoading.css';
 
 function ToDoLoading() {
   return (
-    <span className='loading-container'>
-      <SVGLoading className='loading-icon'/>
-      {/* <img className='loading-icon' alt='Loading' src={flork}></img> */}
-    </span>
+    <>
+      <span className='loading-container'></span>
+      <span className='loading-container'></span>
+      <span className='loading-container'></span>
+    </>
   );
 }
 
